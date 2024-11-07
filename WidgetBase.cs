24a82daaf45cb88a73193358221def5dbabed20e
eelmor <eelmor@gmail.com>
2024-11-07 11:38:40 +0800

    Update version

--- WidgetBase.cs
+++ WidgetBase.cs
@@ -36,7 +36,7 @@ namespace PictureWidget
         }
         public Version Version {
             get {
-                return new Version(1,0,8);
+                return new Version(1,0,9);
             }
         }
 
